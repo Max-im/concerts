07b5cb23c482021a205925fc651c576a0c7ed562
Max Pozhidayev <pogidaevmo@gmail.com>
2017-11-10 03:11:16 +0200

    add main block, decorate block elements

---
+++ src/components/NotFoutd/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NotFound.css';
+
+
+
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      <div className="container">
+        404
+      </div>
+    </div>
+  );
+}
+
+export default NotFound;
---
+++ src/components/Single/Single.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Single.css';
+
+
+
+
+class Single extends Component {
+  render() {
+    return (
+      <div className="single">
+        <div className="container">
+        single
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Single;
